03611f02db21b69dd81949062775e4fe4f77610b
mazo_ <83385567+mazohere@users.noreply.github.com>
2021-12-16 17:52:33 +1100

    Added some comments

--- 99_bobotw.py
+++ 99_bobotw.py
@@ -4,8 +4,12 @@ engine = pyttsx3.init()
 
 # main function
 def beer_on_wall():
+
+    # iteratively counts up from 100 
     for x in reversed(range(1, 100)):
         
+        # prints x amount of bottles of beer on the wall for as long as x > 1, when x = 1 prints 1 bottle of beer on the wall,
+        # this is for grammar reasons
         if x > 1:
             print(x, 'bottles of beer on the wall')
             engine.say(x)
@@ -16,6 +20,7 @@ def beer_on_wall():
             engine.say(x)
             engine.say('bottle of beer on the wall')
             engine.runAndWait()
+    # loops when the function ends by printing the end of the song and re-calling the function
     else:
         print('No bottles of beer on the wall…. Go to the store and buy some more…')
         engine.say('No bottles of beer on the wall…. Go to the store and buy some more…')
